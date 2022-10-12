d5a4e000237a8d4af571b4a8b95394a71bc85ff6
Aqib Ibrahim <aqib.ibrahim.teo@gotoko.co.id>
2022-10-12 12:07:34 +0500

    add price section and dynamic resources in calender

--- src/components/apps/DefaultCalender/DefaultCalender.tsx
+++ src/components/apps/DefaultCalender/DefaultCalender.tsx
@@ -7,14 +7,18 @@ import interactionPlugin from '@fullcalendar/interaction'
 import '@fullcalendar/common/main.css'
 import '@fullcalendar/daygrid/main.css'
 import '@fullcalendar/timegrid/main.css'
-import { INITIAL_EVENTS } from './event-utils'
+import customViewPlugin from './custom-view-file.js'
+import { INITIAL_EVENTS, createEventId } from './event-utils'
 import PageHeader from '../../../Layouts/PageHeader/PageHeader'
 // import { DefaultDatePicker } from '../../../Data/Pages/Forms/FormAdvanceData/DataFormAdvanced'
 import CountUp from 'react-countup'
 import './ReservationList.scss'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
-
+import { Calendar } from '@fullcalendar/core'
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
+import ReactDOM from 'react-dom'
+import { weeksToDays } from 'date-fns'
 interface DemoAppState {
 	weekendsVisible: boolean
 	currentEvents: EventApi[]
@@ -25,6 +29,61 @@ class DefaultCalender extends React.Component<any, DemoAppState> {
 		weekendsVisible: true,
 		currentEvents: [],
 		value: new Date(),
+	}
+	handleDateSelect = (selectInfo: any) => {
+		console.log('date clicked')
+
+		let title = prompt('Please enter a new title for your event')
+		let calendarApi = selectInfo.view.calendar
+
+		calendarApi.unselect() // clear date selection
+
+		if (title) {
+			calendarApi.addEvent({
+				id: createEventId(),
+				title,
+				start: selectInfo.startStr,
+				end: selectInfo.endStr,
+				allDay: selectInfo.allDay,
+			})
+		}
+	}
+	handleEventClick = (clickInfo) => {
+		if (window?.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+			clickInfo.event.remove()
+		}
+	}
+
+	renderEventContent = (eventInfo: {
+		timeText:
+			| string
+			| number
+			| boolean
+			| React.ReactElement<any, string | React.JSXElementConstructor<any>>
+			| React.ReactFragment
+			| React.ReactPortal
+			| null
+			| undefined
+		event: {
+			title:
+				| string
+				| number
+				| boolean
+				| React.ReactElement<any, string | React.JSXElementConstructor<any>>
+				| React.ReactFragment
+				| React.ReactPortal
+				| null
+				| undefined
+		}
+	}) => {
+		return (
+			<>
+				<b>{eventInfo.timeText}</b>
+				<p>
+					<i>{eventInfo.event.title}</i>
+				</p>
+			</>
+		)
 	}
 	// const [state.value, onChange] == useState(new Date);
 	render() {
@@ -37,7 +96,7 @@ class DefaultCalender extends React.Component<any, DemoAppState> {
 							<Row style={{ marginBottom: 10 }}>
 								<Col lg={9} className="rateplan">
 									<div>
-										<span className="d-flex" style={{ gap: 5 }}>
+										<span className="d-flex" style={{ gap: 5, alignItems: 'center' }}>
 											{/* <input
                           className="form-control mb-4"
                           placeholder="Guest Name"
@@ -68,9 +127,24 @@ class DefaultCalender extends React.Component<any, DemoAppState> {
 											<input className="form-control mb-4 mt-4" placeholder="Childeren" />
 											<input className="form-control mb-4 mt-4" placeholder="Rack Rate" />
 											<input className="form-control mb-4 mt-4" placeholder="Promo Code" />
+
+											<i
+												className="fa fa-refresh"
+												style={{
+													color: 'white',
+													border: '1px solid #e9edf4',
+													padding: 10,
+													borderRadius: 4,
+													backgroundColor: '#78b722',
+												}}
+											></i>
+
+											{/* <input className="form-control mb-4 mt-4" placeholder="Promo Code" type= "search"/> */}
 										</span>
 									</div>
 								</Col>
+								{/* <i className="fa-solid fa-magnifying-glass"></i> */}
+
 								<Col lg={3}>
 									<Row className="days-column">
 										{/* <Col lg={2}>
@@ -111,7 +185,7 @@ class DefaultCalender extends React.Component<any, DemoAppState> {
 								</Col>
 							</Row>
 							<Row style={{ marginTop: 30 }}>
-								<Col sm={2}>
+								<Col sm={1}>
 									<div
 										style={{
 											backgroundColor: 'lightgreen',
@@ -123,9 +197,9 @@ class DefaultCalender extends React.Component<any, DemoAppState> {
 											marginBottom: 10,
 										}}
 									></div>
-									<p style={{ textAlign: 'center' }}>Reserved</p>
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>Reserved</p>
 								</Col>
-								<Col sm={2}>
+								<Col sm={1}>
 									<div
 										style={{
 											backgroundColor: 'green',
@@ -137,9 +211,9 @@ class DefaultCalender extends React.Component<any, DemoAppState> {
 											marginBottom: 10,
 										}}
 									></div>
-									<p style={{ textAlign: 'center' }}>Confirmed</p>
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>Confirmed</p>
 								</Col>
-								<Col sm={2}>
+								<Col sm={1}>
 									<div
 										style={{
 											backgroundColor: 'lightskyblue',
@@ -151,9 +225,9 @@ class DefaultCalender extends React.Component<any, DemoAppState> {
 											marginBottom: 10,
 										}}
 									></div>
-									<p style={{ textAlign: 'center' }}>Guranteed</p>
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>Guranteed</p>
 								</Col>
-								<Col sm={2}>
+								<Col sm={1}>
 									<div
 										style={{
 											backgroundColor: 'mediumpurple',
@@ -165,9 +239,9 @@ class DefaultCalender extends React.Component<any, DemoAppState> {
 											marginBottom: 10,
 										}}
 									></div>
-									<p style={{ textAlign: 'center' }}>In House</p>
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>In House</p>
 								</Col>
-								<Col sm={2}>
+								<Col sm={1}>
 									<div
 										style={{
 											backgroundColor: 'grey',
@@ -179,9 +253,9 @@ class DefaultCalender extends React.Component<any, DemoAppState> {
 											marginBottom: 10,
 										}}
 									></div>
-									<p style={{ textAlign: 'center' }}>Departed</p>
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>Departed</p>
 								</Col>
-								<Col sm={2}>
+								<Col sm={1}>
 									<div
 										style={{
 											backgroundColor: 'orange',
@@ -193,84 +267,154 @@ class DefaultCalender extends React.Component<any, DemoAppState> {
 											marginBottom: 10,
 										}}
 									></div>
-									<p style={{ textAlign: 'center' }}>On Hold</p>
+
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>On Hold</p>
+								</Col>
+								<Col sm={1}>
+									<div
+										style={{
+											backgroundColor: 'coral',
+											height: 20,
+											width: 20,
+											borderRadius: 50,
+											marginLeft: 'auto',
+											marginRight: 'auto',
+											marginBottom: 10,
+										}}
+									></div>
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>Reserved</p>
+								</Col>
+								<Col sm={1}>
+									<div
+										style={{
+											backgroundColor: '#e372b1',
+											height: 20,
+											width: 20,
+											borderRadius: 50,
+											marginLeft: 'auto',
+											marginRight: 'auto',
+											marginBottom: 10,
+										}}
+									></div>
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>Reserved</p>
+								</Col>
+								<Col sm={1}>
+									<div
+										style={{
+											backgroundColor: '#d95652',
+											height: 20,
+											width: 20,
+											borderRadius: 50,
+											marginLeft: 'auto',
+											marginRight: 'auto',
+											marginBottom: 10,
+										}}
+									></div>
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>Reserved</p>
+								</Col>
+								<Col sm={1}>
+									<div
+										style={{
+											backgroundColor: '#415b75',
+											height: 20,
+											width: 20,
+											borderRadius: 50,
+											marginLeft: 'auto',
+											marginRight: 'auto',
+											marginBottom: 10,
+										}}
+									></div>
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>Reserved</p>
+								</Col>
+								<Col sm={1}>
+									<div
+										style={{
+											backgroundColor: '#67779f',
+											height: 20,
+											width: 20,
+											borderRadius: 50,
+											marginLeft: 'auto',
+											marginRight: 'auto',
+											marginBottom: 10,
+										}}
+									></div>
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>Reserved</p>
+								</Col>
+								<Col sm={1}>
+									<div
+										style={{
+											backgroundColor: '#d3ecf3',
+											height: 20,
+											width: 20,
+											borderRadius: 50,
+											marginLeft: 'auto',
+											marginRight: 'auto',
+											marginBottom: 10,
+										}}
+									></div>
+									<p style={{ textAlign: 'center', fontSize: 'smaller' }}>Reserved</p>
 								</Col>
 							</Row>
 
 							<Row style={{ marginTop: 20 }}>
-								<Col sm={2} style={{ backgroundColor: 'lightgrey' }}>
-									<Row>
-										<div style={{ backgroundColor: 'blueviolet', height: 35 }}>
-											{/* <p>{new Date()}</p> */}
-											<input
-												type="date"
-												id="start"
-												name="trip-start"
-												value="2018-07-22"
-												min="2018-01-01"
-												max="2018-12-31"
-											></input>
-										</div>
-									</Row>
-
-									<Row>
-										<div style={{ marginTop: 20, paddingLeft: 5, borderBottom: '1px solid grey' }}>
-											<p style={{ fontSize: '1.2rem', marginBottom: 5, fontWeight: 'bold' }}>Rooms</p>
-										</div>
-									</Row>
-									<Row className="roomname">
-										<div style={{ marginTop: 10, paddingLeft: 5 }}>
-											<p style={{ fontSize: '1rem', marginBottom: 5, color: 'white' }}>STDQ</p>
-										</div>
-									</Row>
-									<Row className="roomnumber">
-										<div style={{ marginTop: 10, paddingLeft: 5 }}>
-											<p style={{ fontSize: '1rem', marginBottom: 5 }}>R-098</p>
-										</div>
-									</Row>
-									<Row className="roomnumber">
-										<div style={{ marginTop: 10, paddingLeft: 5 }}>
-											<p style={{ fontSize: '1rem', marginBottom: 5 }}>R-999</p>
-										</div>
-									</Row>
-									<Row className="roomnumber">
-										<div style={{ marginTop: 10, paddingLeft: 5 }}>
-											<p style={{ fontSize: '1rem', marginBottom: 5 }}>R-342</p>
-										</div>
-									</Row>
-									<Row className="roomnumber">
-										<div style={{ marginTop: 10, paddingLeft: 5 }}>
-											<p style={{ fontSize: '1rem', marginBottom: 5 }}>R-444</p>
-										</div>
-									</Row>
-								</Col>
-
-								<Col sm={10}>
+								<Col sm={12} style={{ paddingLeft: 0 }}>
 									<div style={{ height: '343px', overflowY: 'scroll' }}>
 										<FullCalendar
-											plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+											plugins={[resourceTimelinePlugin, interactionPlugin, customViewPlugin]}
 											headerToolbar={{
-												left: 'prev,next today',
+												left: 'prev,title,next',
 												center: 'title',
-												right: 'dayGridMonth,timeGridWeek,timeGridDay',
+												right: 'resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth',
 											}}
-											initialView="dayGridMonth"
+											initialView="resourceTimelineDay"
+											timeZone="UTC"
+											dayHeaderFormat={'weekday: short'}
+											dayMinWidth={200}
+											views={{
+												resourceTimelineMonth: {
+													dayHeaders: true,
+													// allDayContent: { html: '<i>some html</i>' },
+													slotLaneContent: {
+														html: '<div style="display:flex,justify-content:center"><p style="color:green">$140 </p> </div>',
+													},
+													type: 'timeline',
+													duration: { days: 30 },
+													slotDuration: { days: 1 },
+													buttonText: '30 Days',
+													slotLabelFormat: { weekday: 'short', day: 'numeric', omitCommas: true },
+												},
+												resourceTimelineDay: {
+													type: 'timeline',
+													duration: { days: 15 },
+													slotDuration: { days: 1 },
+													buttonText: '15 Days',
+													slotLabelFormat: { weekday: 'short', day: 'numeric', omitCommas: true },
+												},
+												resourceTimelineWeek: {
+													type: 'timeline',
+													duration: { days: 7 },
+													slotDuration: { days: 1 },
+													buttonText: '7 Days',
+													slotLabelFormat: { weekday: 'short', day: 'numeric', omitCommas: true },
+												},
+											}}
+											scrollTime="00:00"
+											height="auto" // will activate stickyHeaderDates automatically!
+											events="https://fullcalendar.io/api/demo-feeds/events.json?single-day&for-resource-timeline"
+											resourceAreaHeaderContent="Rooms (SDTQ)"
+											resources="https://fullcalendar.io/api/demo-feeds/resources.json?with-nesting&with-colors"
+											aspectRatio={1.5}
 											editable={true}
 											selectable={true}
 											selectMirror={true}
 											dayMaxEvents={true}
-											weekends={this.state.weekendsVisible}
-											initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-											// select={this.handleDateSelect}
-											// eventContent={renderEventContent} // custom render function
-											// eventClick={this.handleEventClick}
-											// eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+											resourceAreaWidth="165px"
+											select={this.handleDateSelect}
+											eventContent={this.renderEventContent} // custom render function
+											eventClick={this.handleEventClick}
 											/* you can update a remote database when these fire:
             
             */
-											eventAdd={function () {}}
-											eventChange={function () {}}
-											eventRemove={function () {}}
 										/>
 									</div>
 								</Col>
@@ -308,115 +452,6 @@ class DefaultCalender extends React.Component<any, DemoAppState> {
 							</Row>
 
 							{/* Another Table for dummyh data */}
-
-							<Row style={{ marginTop: 20 }}>
-								<Col sm={2} style={{ backgroundColor: 'lightgrey' }}>
-									<Row>
-										<div style={{ backgroundColor: 'blueviolet', height: 35 }}>
-											<input
-												type="date"
-												id="start"
-												name="trip-start"
-												value="2018-07-22"
-												min="2018-01-01"
-												max="2018-12-31"
-											/>
-										</div>
-									</Row>
-
-									<Row>
-										<div style={{ marginTop: 20, paddingLeft: 5, borderBottom: '1px solid grey' }}>
-											<p style={{ fontSize: '1.2rem', marginBottom: 5, fontWeight: 'bold' }}>Rooms</p>
-										</div>
-									</Row>
-									<Row className="roomname">
-										<div style={{ marginTop: 10, paddingLeft: 5 }}>
-											<p style={{ fontSize: '1rem', marginBottom: 5, color: 'white' }}>Ocean</p>
-										</div>
-									</Row>
-									<Row className="roomnumber">
-										<div style={{ marginTop: 10, paddingLeft: 5 }}>
-											<p style={{ fontSize: '1rem', marginBottom: 5 }}>R-123</p>
-										</div>
-									</Row>
-									<Row className="roomnumber">
-										<div style={{ marginTop: 10, paddingLeft: 5 }}>
-											<p style={{ fontSize: '1rem', marginBottom: 5 }}>R-456</p>
-										</div>
-									</Row>
-									<Row className="roomnumber">
-										<div style={{ marginTop: 10, paddingLeft: 5 }}>
-											<p style={{ fontSize: '1rem', marginBottom: 5 }}>R-334</p>
-										</div>
-									</Row>
-									<Row className="roomnumber">
-										<div style={{ marginTop: 10, paddingLeft: 5 }}>
-											<p style={{ fontSize: '1rem', marginBottom: 5 }}>R-666</p>
-										</div>
-									</Row>
-								</Col>
-
-								<Col sm={10}>
-									<div style={{ height: '343px', overflowY: 'scroll' }}>
-										<FullCalendar
-											plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-											headerToolbar={{
-												left: 'prev,next today',
-												center: 'title',
-												right: 'dayGridMonth,timeGridWeek,timeGridDay',
-											}}
-											initialView="dayGridMonth"
-											editable={true}
-											selectable={true}
-											selectMirror={true}
-											dayMaxEvents={true}
-											weekends={this.state.weekendsVisible}
-											initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-											// select={this.handleDateSelect}
-											// eventContent={renderEventContent} // custom render function
-											// eventClick={this.handleEventClick}
-											// eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-											/* you can update a remote database when these fire:
-            
-            */
-											eventAdd={function () {}}
-											eventChange={function () {}}
-											eventRemove={function () {}}
-										/>
-									</div>
-								</Col>
-							</Row>
-							<Row>
-								<Col className="pr-4" style={{ paddingLeft: 0 }}>
-									<div className="availablerooms">
-										<Table bordered size="sm" style={{ backgroundColor: 'grey' }}>
-											<thead></thead>
-											<tbody>
-												<tr>
-													<td># Room</td>
-													<td>1</td>
-													<td>2</td>
-													<td>3</td>
-													<td>4</td>
-													<td>6</td>
-													<td>5</td>
-													<td>1</td>
-												</tr>
-												<tr>
-													<td>% Occupancy</td>
-													<td>23%</td>
-													<td>48%</td>
-													<td>60%</td>
-													<td>20%</td>
-													<td>50%</td>
-													<td>10%</td>
-													<td>80%</td>
-												</tr>
-											</tbody>
-										</Table>
-									</div>
-								</Col>
-							</Row>
 						</Card.Body>
 					</Card>
 				</div>
--- src/components/apps/DefaultCalender/event-utils.tsx
+++ src/components/apps/DefaultCalender/event-utils.tsx
@@ -1,21 +1,24 @@
 import { EventInput } from '@fullcalendar/react'
+import { color } from 'echarts'
 
 let eventGuid = 0
 let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
 
 export const INITIAL_EVENTS: EventInput[] = [
-  {
-    id: createEventId(),
-    title: 'All-day event',
-    start: todayStr
-  },
-  {
-    id: createEventId(),
-    title: 'Timed event',
-    start: todayStr + 'T12:00:00'
-  }
+	{
+		id: createEventId(),
+		title: 'All-day event',
+		start: todayStr,
+		color: 'red',
+	},
+	{
+		id: createEventId(),
+		title: 'Timed event',
+		start: todayStr + 'T12:00:00',
+		color: 'red',
+	},
 ]
 
 export function createEventId() {
-  return String(eventGuid++)
+	return String(eventGuid++)
 }
