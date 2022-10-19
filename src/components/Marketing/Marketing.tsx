a6a98b83a084842da8b57f2298461e95876ec33b
Aqib Ibrahim <aqib.ibrahim.teo@gotoko.co.id>
2022-10-19 23:10:28 +0500

    create layout for marketing page

--- src/components/Marketing/Marketing.tsx
+++ src/components/Marketing/Marketing.tsx
@@ -6,47 +6,42 @@ import Form from 'react-bootstrap/Form'
 import { Button } from 'react-bootstrap'
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
 const Marketing = () => {
+	const [savePolicy, setSavePolicy] = React.useState(false)
 	return (
 		<Card className="mt-5">
 			<Card.Header>Featured</Card.Header>
 			<Card.Body>
 				<Row className="g-2">
 					<Col md>
-						<FloatingLabel controlId="floatingInputGrid" label="Email address">
+						<FloatingLabel controlId="floatingInputGrid" label="Marketing Fee">
 							<Form.Control type="email" placeholder="name@example.com" />
 						</FloatingLabel>
 					</Col>
-					<Col md>
-						<FloatingLabel controlId="floatingSelectGrid" label="Works with selects">
-							<Form.Select aria-label="Floating label select example">
-								<option>Open this select menu</option>
-								<option value="1">One</option>
-								<option value="2">Two</option>
-								<option value="3">Three</option>
-							</Form.Select>
-						</FloatingLabel>
+					<Col md style={{ display: 'flex', alignItems: 'center', border: '1px solid #e9edf4', borderRadius: '7px' }}>
+						<Form.Check
+							style={{ fontSize: 16, paddingLeft: '2.25rem' }}
+							type="switch"
+							id="custom-switch"
+							label="Activate Marketing"
+							onChange={(e) => {
+								setSavePolicy(e.target.checked)
+							}}
+						/>
 					</Col>
 				</Row>
-				{/* <Row>
-					<Col>
-						{' '}
-						<Form>
-							<fieldset disabled>
-								<Form.Group className="mb-3">
-									<Form.Label htmlFor="disabledTextInput">Marketing Fee</Form.Label>
-									<Form.Control />
-								</Form.Group>
-							</fieldset>
-						</Form>
-					</Col>
-					<Col>This is some text within a card body.</Col>
-				</Row>
-				<Row>
+
+				<Row className="mt-5">
 					<Form.Group className="mb-3">
-						<Form.Check type="checkbox" id="disabledFieldsetCheck" label="Can't check this" />
+						<Form.Check type="checkbox" id="disabledFieldsetCheck" label="I agree terms & condition and policy" />
 					</Form.Group>
-					<Button type="submit">Submit</Button>
-				</Row> */}
+					{savePolicy ? (
+						<Button type="submit">Submit</Button>
+					) : (
+						<Button disabled={true} type="submit">
+							Submit
+						</Button>
+					)}
+				</Row>
 			</Card.Body>
 		</Card>
 	)
